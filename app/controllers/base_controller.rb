c50ac0a7bca1aae77a0cec8e1b4026f6f79a3084
john <johnwu2613@gmail.com>
2017-05-20 22:13:13 +0800

    fix-render-text-in-rails5

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,11 +1,11 @@
 class BaseController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:callback]
-  
+
   def index
   end
 
   def callback
 		msg = LineBotService.new.reply_msg(request)
-		render text: msg
+		render plain: msg
 	end
 end
