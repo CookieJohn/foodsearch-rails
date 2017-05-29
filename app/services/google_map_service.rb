279343d4ffe8649f2df83f54113ffdd4ec2f5378
john <johnwu2613@gmail.com>
2017-05-29 20:21:51 +0800

    use-FIXIE_SOCKS_HOST

--- app/services/google_map_service.rb
+++ app/services/google_map_service.rb
@@ -4,7 +4,7 @@ class GoogleMapService
 	API_URL ||= "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
 	PHOTO_API_URL ||= "https://maps.googleapis.com/maps/api/place/photo?"
 	API_KEY ||= ENV['GOOGLE_API_KEY']
-	FIXIE_URL ||= ENV['FIXIE_URL']
+	FIXIE_SOCKS_HOST ||= ENV['FIXIE_SOCKS_HOST']
 
 	RADIUS ||= 1000
 	RESTAURANT_TYPE ||= 'restaurant'
@@ -15,7 +15,7 @@ class GoogleMapService
 		location = "#{lat},#{lng}"
 		uri = URI("#{API_URL}location=#{location}&radius=#{RADIUS}&type=#{RESTAURANT_TYPE}&opennow=#{OPENNOW}&key=#{API_KEY}")
 
-		uri = Faraday.new(url: uri, proxy: FIXIE_URL)
+		uri = Faraday.new(url: uri, proxy: FIXIE_SOCKS_HOST)
 		res = uri.get
 
 		results = JSON.parse(res.body)['results'].first(5)
@@ -26,7 +26,7 @@ class GoogleMapService
 	def photo size, photoreference
 
 		uri = URI("#{PHOTO_API_URL}maxheight=#{size}&photoreference=#{photoreference}&key=#{API_KEY}")
-		uri = Faraday.new(url: uri, proxy: FIXIE_URL)
+		uri = Faraday.new(url: uri, proxy: FIXIE_SOCKS_HOST)
 		res = uri.get
 		puts res.body
 		return JSON.parse(res.body)
@@ -36,7 +36,7 @@ class GoogleMapService
 		location = "#{lat},#{lng}"
 		uri = URI("#{API_URL}location=#{location}&radius=#{RADIUS}&type=#{RESTAURANT_TYPE}&opennow=#{OPENNOW}&key=#{API_KEY}")
 
-		uri = Faraday.new(url: uri)
+		uri = Faraday.new(url: uri, proxy: FIXIE_SOCKS_HOST)
 		res = uri.get
 
 		return res.body
