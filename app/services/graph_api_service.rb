03b170b36d85eb1dfe4d9f6f9fb4e053ac14e05f
john <johnwu2613@gmail.com>
2017-05-30 22:57:26 +0800

    add-auto-refresh-token

--- app/services/graph_api_service.rb
+++ app/services/graph_api_service.rb
@@ -2,11 +2,12 @@ require 'koala'
 
 class GraphApiService
 	# API_URL ||= "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
-	TOKEN ||= 'EAACEdEose0cBAJUo9MEk8A3RDFXa6828eFsxHHzMYbpj3jH8m6DyWV5CzJHyNArTKK8IZBZBM4bclHuV2rElEwmiDHmxikZAVZC04vCYHZBujWVKQl896mTY1GFlyR8e2l9Qmso3wtjm9lwW5DLKYzfdgaC2s9chKgDexZBAUP2AV3RsfYjF5yB66FCB7Uyv0ZD'
 
 	attr_accessor :graph
 	def initialize
-		graph = Koala::Facebook::API.new(TOKEN)
+		@oauth = Koala::Facebook::OAuth.new
+		oauth_access_token = @oauth.get_app_access_token
+		self.graph = Koala::Facebook::API.new(oauth_access_token)
 	end
 
 	# def place_search lat, lng
@@ -21,20 +22,17 @@ class GraphApiService
 	# end
 
 	def search_restaurant restaurant_name
-		graph = Koala::Facebook::API.new(TOKEN)
 		results = graph.search(restaurant_name, type: :page)
 		target_result = results.first
 
 		return target_result
 	end
 	def get_location id
-		graph = Koala::Facebook::API.new(TOKEN)
 		result = graph.get_connections(id, "?fields=location,name,category,overall_star_rating,rating_count,picture")
 		return result
 	end
 
 	def photo id
-		graph = Koala::Facebook::API.new(TOKEN)
 		result = graph.get_picture_data(id, type: :large)#['data']['url']
 		url = result.present? ? result['data']['url'] : ""
 		return url
@@ -44,7 +42,6 @@ class GraphApiService
 		# location = "#{lat},#{lng}"
 		# uri = URI("#{API_URL}location=#{location}&radius=#{RADIUS}&type=#{RESTAURANT_TYPE}&opennow=#{OPENNOW}&key=#{API_KEY}")
 		# res = uri.get
-		graph = Koala::Facebook::API.new(TOKEN)
 		profile = graph.get_object("me")
 
 		return profile
--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,6 +1,6 @@
 Koala.configure do |config|
-  # config.app_id = Settings.facebook.app_id
-  # config.app_secret = Settings.facebook.app_secret
+  config.app_id = Settings.facebook.app_id
+  config.app_secret = Settings.facebook.app_secret
   # config.app_access_token = Settings.facebook.app_access_token
   # config.access_token = Settings.facebook.access_token
 end
