6337781aa57d3efe61417e76d878007886a11a20
john <johnwu2613@gmail.com>
2017-05-31 10:15:24 +0800

    fix-min-score

--- app/services/graph_api_service.rb
+++ app/services/graph_api_service.rb
@@ -2,7 +2,7 @@ require 'koala'
 
 class GraphApiService
 	DEFAULT_DISTANCE ||= 500
-	DEFAULT_MIN_SCORE ||= 4
+	DEFAULT_MIN_SCORE ||= 3.9
 	DEFAULT_FIELDS ||= 'location,name,overall_star_rating,rating_count,category,phone,link'
 
 	attr_accessor :graph
@@ -16,7 +16,7 @@ class GraphApiService
 		location = "#{lat},#{lng}"
 		facebook_results = graph.search('restaurant', type: :place,center: location, distance: DEFAULT_DISTANCE, fields: DEFAULT_FIELDS)
 		results = facebook_results.sort_by { |r| r['overall_star_rating'].to_i }.reverse
-		results = results.select { |r| r['overall_star_rating'].to_i > DEFAULT_MIN_SCORE }.sample(5)
+		results = results.select { |r| r['overall_star_rating'].to_f > DEFAULT_MIN_SCORE }.sample(5)
 		return results
 	end
 
