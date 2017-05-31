e3ec0e8edbc9787033c7c0432725b138bcb8afab
john <johnwu2613@gmail.com>
2017-05-31 22:34:55 +0800

    validate-link

--- app/services/graph_api_service.rb
+++ app/services/graph_api_service.rb
@@ -25,6 +25,7 @@ class GraphApiService
 		location = "#{lat},#{lng}"
 		facebook_results = graph.search('restaurant', type: :place,center: location, distance: max_distance, fields: DEFAULT_FIELDS, locale: DEFAULT_LOCALE)
 		results = facebook_results.reject { |r| REJECT_PRICE.include?(r['price_range'].to_s) }
+		results = results.select { |r| r['link'].to_s.present? }
 		results = results.select { |r| r['overall_star_rating'].to_f >= min_score }
 		results = results.sort_by { |r| r['overall_star_rating'].to_f }.reverse
 
