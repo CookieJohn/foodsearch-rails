c8850f7504a2fd038d96a3e32c20893fd1dc7eb7
john <johnwu2613@gmail.com>
2017-06-01 11:33:52 +0800

    add-log

--- app/services/line_bot_service.rb
+++ app/services/line_bot_service.rb
@@ -118,10 +118,11 @@ class LineBotService
       match_google_result = {'score' => 0.0, 'match_score' => 0.0}
       google_results.each do |r|
         match_score = jarow.getDistance(r['name'],name).to_f
-        if match_score >= 0.5 && match_score > match_google_result['match_score']
+        if match_score >= 0.6 && match_score > match_google_result['match_score']
           match_google_result['score'] = r['rating'].to_f
           match_google_result['match_score'] = match_score
         end
+        Rails.logger.info "判斷字串：#{r['name']}m #{name}, 判斷分數：#{match_score}"
       end
 
       text = ""
