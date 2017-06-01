b2b5403de9999ce2b3b49d37dab6568c8a1dde27
john <johnwu2613@gmail.com>
2017-06-01 22:16:05 +0800

    change-size

--- app/services/graph_api_service.rb
+++ app/services/graph_api_service.rb
@@ -41,7 +41,7 @@ class GraphApiService
 	end
 
 	def get_photo id
-		"https://graph.facebook.com/#{id}/picture??width=400&height=300"
+		"https://graph.facebook.com/#{id}/picture??width=300&height=200"
 	end
 
 	def get_current_open_time hours
