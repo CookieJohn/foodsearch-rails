9ab0207b06708072e7e4a4054d9aa29274ff2016
john <johnwu2613@gmail.com>
2017-06-01 11:23:07 +0800

    up-match_score

--- app/services/line_bot_service.rb
+++ app/services/line_bot_service.rb
@@ -118,7 +118,7 @@ class LineBotService
       match_google_result = {'score' => 0, 'match_score' => 0}
       google_results.each do |r|
         match_score = jarow.getDistance( r['name'], name ).to_f
-        if match_score >= 0.4 && match_score > match_google_result['match_score']
+        if match_score >= 0.5 && match_score > match_google_result['match_score']
           match_google_result['score'] = r['rating']
           match_google_result['match_score'] = match_score
         end
