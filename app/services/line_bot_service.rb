9d47b86bbc29a468006ed73fce47f05dec5cec81
john <johnwu2613@gmail.com>
2017-06-01 23:06:07 +0800

    fix-i18n

--- app/services/line_bot_service.rb
+++ app/services/line_bot_service.rb
@@ -164,7 +164,7 @@ class LineBotService
   def handle_with_commands msg, command, user
     case command
     when I18n.t('common.user')
-      "#{I18n.t('common.user')}#{I18n.t('common.setting')}：\n#{I18n.t('common.radius')}：#{user.try(:max_distance)}\n#{I18n.t('common.point')}：#{user.try(:min_score)}\n#{I18n.t('common.random')}：#{user.try(:random_type)}"
+      "#{I18n.t('common.user')}#{I18n.t('common.setting')}：\n#{I18n.t('common.radius')}：#{user.try(:max_distance)}\n#{I18n.t('common.point')}：#{user.try(:min_score)}\n#{I18n.t('common.random')}：#{user.random_type ? I18n.t('common.open') : I18n.t('common.close')}"
     when I18n.t('common.command')
       "#{I18n.t('common.command')}：\n#{I18n.t('common.setting')}#{I18n.t('common.random')}：#{I18n.t('common.random')}true/false\n#{I18n.t('common.radius')}500(500~50000)\n#{I18n.t('common.point')}3.8 (3~5 接受小數第一位)"
     when I18n.t('common.random')
