1887c92542bdc9374268112153dbf8b7efc4c407
john <johnwu2613@gmail.com>
2017-06-08 00:08:39 +0800

    add-facebook-webhook

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,12 +1,17 @@
 class BaseController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:callback, :facebook_callback]
 
-  def index
-  end
+  # def index
+  # end
 
   def callback
 		msg = LineBotService.new.reply_msg(request)
 		render plain: msg
+	end
+
+	def webhook
+
+		render plain: params['hub.challenge'], status: 200
 	end
 
 	def facebook_callback
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # root 'base#index'
+  get 'webhook' => 'base#webhook'
   post 'callback' => 'base#callback'
   post 'facebook_callback' => 'base#facebook_callback'
 end
