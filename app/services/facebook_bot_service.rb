71f7d29086fb2236ccd4245bb6a207341ea3cf79
john <johnwu2613@gmail.com>
2017-06-08 17:53:38 +0800

    basic-fb-bot

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -1,13 +1,32 @@
+require 'net/http'
+
 class FacebookBotService
 
 	def initialize
-    self.graph  ||= GraphApiService.new
-    self.google ||= GoogleMapService.new
-    self.common ||= CommonService.new
+    # self.graph  ||= GraphApiService.new
+    # self.google ||= GoogleMapService.new
+    # self.common ||= CommonService.new
   end
 
   def reply_msg request
     body = request.body
+    senderID = ['entry'].first['messaging'].first['sender']['id']
+    recipientID = params['entry'].first['messaging'].first['recipient']['id']
+    timeOfMessage = params['entry'].first['time']
+    message = params['entry'].first['messaging'].first['message']['text']
+
+    messageData = {
+      recipient: {
+        id: recipientID
+      },
+      message: {
+        text: message
+      }
+    };
+
+    qs = { access_token: PAGE_ACCESS_TOKEN },
+    uri = URI('https://graph.facebook.com/v2.9/me/messages')
+    res = Net::HTTP.post_form(uri, 'q' => ['ruby', 'perl'], 'max' => '50')
   	return JSON.parse(request.body)
   end
 
