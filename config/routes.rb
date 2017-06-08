2514c7ffbab721ef25f169ea7514796ca0cb05b5
john <johnwu2613@gmail.com>
2017-06-08 23:43:52 +0800

    stop-self-ddos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'base#index'
   post 'callback' => 'base#callback'
-  get 'webhook' => 'base#webhook'
-  post 'webhook' => 'base#facebook_callback'
+  # get 'webhook' => 'base#webhook'
+  # post 'webhook' => 'base#facebook_callback'
 end
