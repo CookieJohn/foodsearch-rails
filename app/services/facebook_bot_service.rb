45779381d8e2bac8b920e8e73f2b9e001cbd8c31
john <johnwu2613@gmail.com>
2017-06-09 22:48:16 +0800

    refactoring

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -11,29 +11,18 @@ class FacebookBotService
 
   def reply_msg request
     body = JSON.parse(request.body.read)
-
     entries = body['entry']
-    Rails.logger.info "entries: #{entries}"
-    page = body['object']
-    Rails.logger.info "page: #{page}"
 
-    # # senderID = entries.first['messaging'].first['sender']['id']
-    # # message = entries.first['messaging'].first['message']['text']
-    # # Rails.logger.info "entries: #{entries}"
-    if page == 'page'
+    if body.dig('object') == 'page'
       entries.each do |entry|
         entry['messaging'].each do |message|
           reveive_message = message['message']['text'].to_s
           senderID = message['sender']['id']
-          Rails.logger.info "reveive_message: #{reveive_message}"
-          Rails.logger.info "senderID: #{senderID}"
           if reveive_message.present?
             messageData = self.text_format(senderID, reveive_message)
             token = Settings.facebook.page_access_token
             uri = "https://graph.facebook.com/v2.6/me/messages?access_token=#{token}"
-    #         # res = Net::HTTP.post_form(uri, messageData)
             res = HTTParty.post(uri, body: messageData)
-    #         # Rails.logger.info "res: #{JSON.parse(res)}"
           end
         end
       end
