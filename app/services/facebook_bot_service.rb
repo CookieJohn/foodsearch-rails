08b116bd1da78f826498e32a2ae168222be542fb
john <johnwu2613@gmail.com>
2017-06-09 22:45:12 +0800

    try

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -23,16 +23,16 @@ class FacebookBotService
     if page == 'page'
       entries.each do |entry|
         entry['messaging'].each do |message|
-          message = message['message']['text'].to_s
+          reveive_message = message['message']['text'].to_s
           senderID = message['sender']['id']
-          Rails.logger.info "message: #{message}"
+          Rails.logger.info "reveive_message: #{reveive_message}"
           Rails.logger.info "senderID: #{senderID}"
           if message.present?
-            messageData = self.text_format(senderID, message)
+            messageData = self.text_format(senderID, reveive_message)
             token = Settings.facebook.page_access_token
             uri = "https://graph.facebook.com/v2.6/me/messages?access_token=#{token}"
     #         # res = Net::HTTP.post_form(uri, messageData)
-    #         res = HTTParty.post(uri, body: messageData)
+            res = HTTParty.post(uri, body: messageData)
     #         # Rails.logger.info "res: #{JSON.parse(res)}"
           end
         end
