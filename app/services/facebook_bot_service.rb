e114015f5d8e49735c45e6b61ffc558275250d7f
john <johnwu2613@gmail.com>
2017-06-09 23:56:43 +0800

    log

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -40,6 +40,7 @@ class FacebookBotService
 
             messageData = self.generic_elements(senderID, fb_results, google_results)
             res = HTTParty.post(uri, body: messageData)
+            Rails.logger.info "res: #{JSON.parse(res)}"
           elsif reveive_message.present?
             messageData = self.text_format(senderID, reveive_message)
             res = HTTParty.post(uri, body: messageData)
