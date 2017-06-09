195c3b986fa5589b536bc169dc3a7f931b171703
john <johnwu2613@gmail.com>
2017-06-09 16:45:10 +0800

    restore-facebook-bot

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -14,23 +14,23 @@ class FacebookBotService
 
     entries = body['entry']
 
-    senderID = entries.first['messaging'].first['sender']['id']
-    message = entries.first['messaging'].first.dig('message','text')
+    # senderID = entries.first['messaging'].first['sender']['id']
+    # message = entries.first['messaging'].first['message']['text']
 
-    # entries.each do |entry|
-    #   entry['messaging'].each do |message|
-    #     message   = message['message']['text'].to_s
-    #     senderID = message['sender']['id']
-    #   end
-    # end
-
-    messageData = self.text_format(senderID, message)
-
-    token = Settings.facebook.page_access_token
-    uri = "https://graph.facebook.com/v2.6/me/messages?access_token=#{token}"
-    # res = Net::HTTP.post_form(uri, messageData)
-    # res = HTTParty.post(uri, body: messageData)
-    # Rails.logger.info res
+    entries.each do |entry|
+      entry['messaging'].each do |message|
+        message = ''
+        message   = message['message']['text'].to_s
+        senderID = message['sender']['id']
+        if message.present?
+          messageData = self.text_format(senderID, message)
+          token = Settings.facebook.page_access_token
+          uri = "https://graph.facebook.com/v2.6/me/messages?access_token=#{token}"
+          # res = Net::HTTP.post_form(uri, messageData)
+          res = HTTParty.post(uri, body: messageData)
+        end
+      end
+    end
   end
 
   def text_format id, text
