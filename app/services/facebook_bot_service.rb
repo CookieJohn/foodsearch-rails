b6cbcfebd1424b956bbd267f6ae99c106302e722
john <johnwu2613@gmail.com>
2017-06-09 23:13:52 +0800

    add-basic-service

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -3,9 +3,9 @@ require 'httparty'
 class FacebookBotService
 
 	def initialize
-    # self.graph  ||= GraphApiService.new
-    # self.google ||= GoogleMapService.new
-    # self.common ||= CommonService.new
+    self.graph  ||= GraphApiService.new
+    self.google ||= GoogleMapService.new
+    self.common ||= CommonService.new
   end
 
   def reply_msg request
@@ -14,6 +14,10 @@ class FacebookBotService
 
     token = Settings.facebook.page_access_token
     uri = "https://graph.facebook.com/v2.6/me/messages?access_token=#{token}"
+
+    user_id = event['source']['userId']
+    User.create!(line_user_id: user_id) if !User.exists?(line_user_id: user_id)
+    user = User.find_by(line_user_id: user_id)
 
     if body.dig('object') == 'page'
       entries.each do |entry|
@@ -27,6 +31,12 @@ class FacebookBotService
             lng = location.dig('payload','coordinates','long')
           end
           if lat.present? && lng.present?
+            fb_results = graph.search_places(lat, lng, user)
+            google_results = ''
+            if user.get_google_result
+              keywords = fb_results.map {|f| f['name']}
+              google_results = google.search_places(lat, lng, user, keywords)
+            end
             messageData = self.text_format(senderID, "#{lat},#{lng}")
             res = HTTParty.post(uri, body: messageData)
           elsif reveive_message.present?
