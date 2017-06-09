01107df621b771a3411606c606f67bc7206d6120
john <johnwu2613@gmail.com>
2017-06-09 23:03:35 +0800

    add-lat-lng

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -12,15 +12,21 @@ class FacebookBotService
     body = JSON.parse(request.body.read)
     entries = body['entry']
 
+    token = Settings.facebook.page_access_token
+    uri = "https://graph.facebook.com/v2.6/me/messages?access_token=#{token}"
+
     if body.dig('object') == 'page'
       entries.each do |entry|
         entry['messaging'].each do |message|
           reveive_message = message['message']['text'].to_s
           senderID = message['sender']['id']
-          if reveive_message.present?
+          lat = message['message']['attachments'].first['payload']['coordinates']['lat']
+          lng = message['message']['attachments'].first['payload']['coordinates']['long']
+          if lat.present? && lng.present?
+            messageData = self.text_format(senderID, "#{lat},#{lng}")
+            res = HTTParty.post(uri, body: messageData)
+          elsif reveive_message.present?
             messageData = self.text_format(senderID, reveive_message)
-            token = Settings.facebook.page_access_token
-            uri = "https://graph.facebook.com/v2.6/me/messages?access_token=#{token}"
             res = HTTParty.post(uri, body: messageData)
           end
         end
