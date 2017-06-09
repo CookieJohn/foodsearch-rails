fe3150e1598e8c4efa867cb3dcf283942096e462
john <johnwu2613@gmail.com>
2017-06-09 22:30:08 +0800

    remove-parse

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -10,28 +10,28 @@ class FacebookBotService
   end
 
   def reply_msg request
-    body = JSON.parse(request.body)
+    body = request.body.read
 
     entries = body['entry']
     page = body['object']
 
     # senderID = entries.first['messaging'].first['sender']['id']
     # message = entries.first['messaging'].first['message']['text']
-    Rails.logger.info "entries: #{entries}"
+    # Rails.logger.info "entries: #{entries}"
     if page == 'page'
       entries.each do |entry|
         entry['messaging'].each do |message|
           message = message['message']['text'].to_s
           senderID = message['sender']['id']
-          Rails.logger.info "message: #{message}"
-          Rails.logger.info "senderID: #{senderID}"
+          # Rails.logger.info "message: #{message}"
+          # Rails.logger.info "senderID: #{senderID}"
           if message.present?
             messageData = self.text_format(senderID, message)
             token = Settings.facebook.page_access_token
             uri = "https://graph.facebook.com/v2.6/me/messages?access_token=#{token}"
             # res = Net::HTTP.post_form(uri, messageData)
             res = HTTParty.post(uri, body: messageData)
-            Rails.logger.info "res: #{JSON.parse(res)}"
+            # Rails.logger.info "res: #{JSON.parse(res)}"
           end
         end
       end
