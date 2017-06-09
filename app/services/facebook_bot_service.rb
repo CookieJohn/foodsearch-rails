b7e8dc80a23938fc1a7bcb1b0e7da6bef1738d2f
john <johnwu2613@gmail.com>
2017-06-10 00:37:21 +0800

    chnage-width

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -136,7 +136,7 @@ class FacebookBotService
           type: 'template',
           payload: {
             template_type: 'generic',
-            image_aspect_ratio: 'square',
+            image_aspect_ratio: 'horizontal',
             elements: columns
           }
         }
