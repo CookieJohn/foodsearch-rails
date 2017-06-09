8c2e8e51de5016790217e16d16737a3fb605ea7b
john <johnwu2613@gmail.com>
2017-06-09 22:36:52 +0800

    try

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -13,29 +13,30 @@ class FacebookBotService
     body = JSON.parse(request.body.read)
 
     entries = body['entry']
-    page = body['object']
+    Rails.logger.info "entries: #{entries}"
+    # page = body['object']
 
-    # senderID = entries.first['messaging'].first['sender']['id']
-    # message = entries.first['messaging'].first['message']['text']
-    # Rails.logger.info "entries: #{entries}"
-    if page == 'page'
-      entries.each do |entry|
-        entry['messaging'].each do |message|
-          message = message['message']['text'].to_s
-          senderID = message['sender']['id']
-          # Rails.logger.info "message: #{message}"
-          # Rails.logger.info "senderID: #{senderID}"
-          if message.present?
-            messageData = self.text_format(senderID, message)
-            token = Settings.facebook.page_access_token
-            uri = "https://graph.facebook.com/v2.6/me/messages?access_token=#{token}"
-            # res = Net::HTTP.post_form(uri, messageData)
-            res = HTTParty.post(uri, body: messageData)
-            # Rails.logger.info "res: #{JSON.parse(res)}"
-          end
-        end
-      end
-    end
+    # # senderID = entries.first['messaging'].first['sender']['id']
+    # # message = entries.first['messaging'].first['message']['text']
+    # # Rails.logger.info "entries: #{entries}"
+    # if page == 'page'
+    #   entries.each do |entry|
+    #     entry['messaging'].each do |message|
+    #       message = message['message']['text'].to_s
+    #       senderID = message['sender']['id']
+    #       # Rails.logger.info "message: #{message}"
+    #       # Rails.logger.info "senderID: #{senderID}"
+    #       if message.present?
+    #         messageData = self.text_format(senderID, message)
+    #         token = Settings.facebook.page_access_token
+    #         uri = "https://graph.facebook.com/v2.6/me/messages?access_token=#{token}"
+    #         # res = Net::HTTP.post_form(uri, messageData)
+    #         res = HTTParty.post(uri, body: messageData)
+    #         # Rails.logger.info "res: #{JSON.parse(res)}"
+    #       end
+    #     end
+    #   end
+    # end
   end
 
   def text_format id, text
