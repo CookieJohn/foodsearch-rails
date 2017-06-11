49822ec2e222ac478ee609598bc356fee68041c0
john <johnwu2613@gmail.com>
2017-06-11 23:45:13 +0800

    get-location-data-after-draggable

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -9,4 +9,21 @@ function initMap() {
     map: map,
     draggable: true,
   });
+  var geocoder = new google.maps.Geocoder();
+  google.maps.event.addListener(marker, 'dragend', function (event) {
+    // document.getElementById("google_address").innerHTML = "地址：" + results[0].formatted_address;
+    document.getElementById("lat").innerHTML = "經度：" + event.latLng.lat();
+    document.getElementById("lng").innerHTML = "緯度：" +event.latLng.lng();
+    // alert(document.getElementById("lat").value);
+    geocoder.geocode({
+    'latLng': event.latLng
+    }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          document.getElementById("google_address").innerHTML = "地址：" + results[0].formatted_address;
+          // alert(results[0].formatted_address);
+        }
+      }
+    });
+  });
 }
