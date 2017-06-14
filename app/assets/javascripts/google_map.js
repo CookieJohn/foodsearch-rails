5d182b9ab7477964f8069391dfc8d40725f2547a
john <johnwu2613@gmail.com>
2017-06-14 22:04:13 +0800

    find-devise-location

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -2,13 +2,43 @@ function initMap() {
   var uluru = {lat: 25.059651, lng: 121.533380};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
-    center: uluru,
+    // center: uluru,
   });
+  // var infoWindow = new google.maps.InfoWindow({map: map});
+  var infoWindow = '';
   var marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-    draggable: true,
-  });
+      map: map,
+      draggable: true,
+    });
+    // Try HTML5 geolocation.
+    $('#loading').show();
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        // map.setCenter(pos);
+
+        // infoWindow.setPosition(pos);
+        // infoWindow.setContent('您的位置');
+
+        marker.setPosition(pos);
+        map.setCenter(pos);
+        $('#loading').hide();
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter(), map, marker, uluru);
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter(), map, marker, uluru);
+    }
+
+  // var marker = new google.maps.Marker({
+  //   position: uluru,
+  //   map: map,
+  //   draggable: true,
+  // });
   var geocoder = new google.maps.Geocoder();
   var loading = document.getElementById('loading');
   google.maps.event.addListener(marker, 'dragend', function (event) {
@@ -45,4 +75,14 @@ function send_post() {
       $('#loading').hide();
     }
   });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos, map, marker, uluru) {
+  // infoWindow.setPosition(pos);
+  // infoWindow.setContent(browserHasGeolocation ?
+  //                       '讀取所在位置失敗。' :
+  //                       '您的裝置不支援定位。');
+  marker.setPosition(uluru);
+  map.setCenter(uluru);
+  $('#loading').hide();
 }
