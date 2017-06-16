675303d71cd47b1fea6322bfdb7273c649d27bf8
john <johnwu2613@gmail.com>
2017-06-16 11:19:05 +0800

    add-ga-track

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def i18n_button model_name, locale: 'zh-TW'
 		I18n.t("button.#{model_name}", locale: locale)
 	end
+
+	def on_production?
+		Rails.env.production?
+	end
 end
