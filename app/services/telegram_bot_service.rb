ce7c8523b7bbf191765dfb84ee5bd12e5f25cade
john <johnwu2613@gmail.com>
2017-08-19 00:25:16 +0800

    basic-response

--- app/services/telegram_bot_service.rb
+++ app/services/telegram_bot_service.rb
@@ -9,8 +9,15 @@ class TelegramBotService < BaseService
   end
 
   def reply_msg
-    body = request.body.read
-    msg = body.dig('message','text')
-    true
+    body = JSON.parse(request.raw_post)
+    msg = body['message']['text']
+    chat_id = body['message']['from']['id']
+
+    if msg.present?
+      response_api = "#{API_URL}sendMessage"
+      messageData = {chat_id: chat_id, text: msg}
+
+      results = http_post(response_api, messageData)
+    end
   end
 end
