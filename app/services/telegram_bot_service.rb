9736d68b711b4f6ec3f7b24d076d8ae2d1d436db
john <johnwu2613@gmail.com>
2017-08-19 07:47:31 +0800

    add-text-format

--- app/services/telegram_bot_service.rb
+++ app/services/telegram_bot_service.rb
@@ -2,22 +2,28 @@ class TelegramBotService < BaseService
   TOKEN ||= Settings.telegram.token
   API_URL ||= "https://api.telegram.org/bot#{TOKEN}/"
 
-  attr_accessor :request
+  attr_accessor :request, :chat_id
   def initialize request
     # self.user ||= nil
     self.request ||= request
+    self.chat_id ||= chat_id
   end
 
   def reply_msg
     body = JSON.parse(request.raw_post)
     msg = body['message']['text']
-    chat_id = body['message']['from']['id']
+    self.chat_id = body['message']['from']['id']
 
-    if msg.present?
-      response_api = "#{API_URL}sendMessage"
-      messageData = {chat_id: chat_id, text: msg}
+    next unless chat_id.present? && msg.present?
 
-      results = http_post(response_api, messageData)
-    end
+    response_api = "#{API_URL}sendMessage"
+    response = text_format(msg)
+
+    results = http_post(response_api, response)
+  end
+
+  def text_format text
+    { chat_id: chat_id, 
+      text: msg}
   end
 end
