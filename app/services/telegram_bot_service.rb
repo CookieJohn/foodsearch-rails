40eb69aed5cf892f39cc1368f234d982fc3112a5
john <johnwu2613@gmail.com>
2017-08-18 23:50:07 +0800

    fix-request

--- app/services/telegram_bot_service.rb
+++ app/services/telegram_bot_service.rb
@@ -2,12 +2,15 @@ class TelegramBotService < BaseService
   TOKEN ||= Settings.telegram.token
   API_URL ||= "https://api.telegram.org/bot#{TOKEN}/"
 
+  attr_accessor :request
   def initialize request
     # self.user ||= nil
     self.request ||= request
   end
 
   def reply_msg
+    body = request.body.read
+    msg = body.dig('message','text')
     true
   end
 end
