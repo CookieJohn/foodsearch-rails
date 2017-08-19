4d244d079a831275bca409afebe20dd874828c26
john <johnwu2613@gmail.com>
2017-08-19 08:15:35 +0800

    add-location-button

--- app/services/telegram_bot_service.rb
+++ app/services/telegram_bot_service.rb
@@ -29,7 +29,11 @@ class TelegramBotService < BaseService
 
   def key_board_button_format text
     { chat_id: chat_id, 
-      text: '請給我位置',
-      request_location: true }
+      text: text,
+      keyboard: [
+        [text: '請給我位置', request_location: true]],
+      chat_id: chat_id, 
+      resize_keyboard: true,
+      one_time_keyboard: true }
   end
 end
