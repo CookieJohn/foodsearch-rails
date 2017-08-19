f569b60b8018e3157bc6ec5edb43c1098f7d77e7
john <johnwu2613@gmail.com>
2017-08-19 08:52:30 +0800

    basic-text

--- app/services/telegram_bot_service.rb
+++ app/services/telegram_bot_service.rb
@@ -14,7 +14,7 @@ class TelegramBotService < BaseService
   def reply_msg
     body = JSON.parse(request.raw_post)
     msg = body.dig('message','text')
-    self.chat_id = body.dig('message','from','id')
+    self.chat_id = body.dig('message','chat','id')
     lat = body.dig('message','location','latitude')
     lng = body.dig('message','location','longitude')
 
@@ -22,8 +22,9 @@ class TelegramBotService < BaseService
       if lat.present?
         # keyword = user.last_search['keyword'].present? ? user.last_search['keyword'] : nil
         fb_results = graph.search_places(lat, lng, user, 10, nil, nil)
+        generic_format = generic_elements(fb_results)
         if fb_results.size > 0 
-          response = text_format(lat)
+          response = text_format(generic_format)
         else
           response = text_format('no_result')
         end
@@ -51,5 +52,55 @@ class TelegramBotService < BaseService
 
   def location_button_format
     [ text: '請告訴我您的位置', request_location: true ]
+  end
+
+  def generic_elements results=nil
+    columns = []
+
+    results.each do |result|
+      id = result['id']
+      name = result['name'][0, 80]
+      lat = result['location']['latitude']
+      lng = result['location']['longitude']
+      street = result['location']['street'] || ""
+      rating = result['overall_star_rating']
+      rating_count = result['rating_count']
+      # phone = result.dig('phone').present? ? result['phone'].gsub('+886','0') : "00000000"
+      link_url = result['link'] || result['website']
+      category = result['category']
+      category_list = result['category_list']
+      hours = result['hours']
+      distance = result['distance'].present? ? "#{result['distance']}公尺" : ''
+
+      description = category
+      category_list.sample(2).each do |c|
+        description += ", #{c['name']}" if c['name'] != category && !REJECT_CATEGORY.any? {|r| c['name'].include?(r) }
+        # new_category = Category.create!(facebook_id: c['id'], facebook_name: c['name']) if !category_lists.any? {|cl| cl.include?(c['id']) }
+      end
+      image_url = graph.get_photo(id)
+
+      actions = []
+      actions << button(safe_url(link_url), I18n.t('button.official'))
+      actions << button(safe_url(google.get_map_link(lat, lng, name, street)),I18n.t('button.location'))
+      actions << button(safe_url(google.get_google_search(name)),I18n.t('button.related_comment'))
+      today_open_time = hours.present? ? graph.get_current_open_time(hours) : I18n.t('empty.no_hours')
+
+      text = "#{I18n.t('facebook.score')}：#{rating}#{I18n.t('common.score')}/#{rating_count}#{I18n.t('common.people')}" if rating.present?
+      text += "\n#{description}"
+      text += "\n#{today_open_time}"
+      text += "\n#{distance}"
+      # text += "\n#{phone}"
+
+      text = text[0, 80]
+
+      columns << {
+        title: name,
+        subtitle: text,
+        image_url: image_url,
+        buttons: actions
+      }
+    end
+
+    return text
   end
 end
