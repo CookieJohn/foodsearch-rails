161360a0fc8d7d7203fa56d32aee3a444f92c172
John <johnwu2613@gmail.com>
2017-10-28 22:44:50 +0800

    remove-i18n-params

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -2,54 +2,54 @@ require 'fuzzystringmatch'
 require 'httparty'
 
 class BaseService
-	REJECT_CATEGORY ||= I18n.t('settings.facebook.reject_category')
+  REJECT_CATEGORY ||= I18n.t('settings.facebook.reject_category')
 
-	@jarow ||= FuzzyStringMatch::JaroWinkler.create(:native)
+  @jarow ||= FuzzyStringMatch::JaroWinkler.create(:native)
 
-	def safe_url link
-		uri = URI.encode(link)
+  def safe_url link
+    uri = URI.encode(link)
     uri = URI.parse(uri)
-	end
+  end
 
-	def fuzzy_match compare_a, compare_b
-		@jarow.getDistance(compare_a,compare_b).to_f
-	end
+  def fuzzy_match compare_a, compare_b
+    @jarow.getDistance(compare_a,compare_b).to_f
+  end
 
-	def http_get get_uri, params=nil
-		uri = safe_url(get_uri)
-		res = Net::HTTP.get_response(uri)
-		results = JSON.parse(res.body)['results']
-	end
+  def http_get get_uri, params=nil
+    uri = safe_url(get_uri)
+    res = Net::HTTP.get_response(uri)
+    results = JSON.parse(res.body)['results']
+  end
 
-	def http_post post_uri, params
-		res = HTTParty.post(post_uri, body: params.to_json, headers: { 'Content-Type' => 'application/json' })
-	end
+  def http_post post_uri, params
+    res = HTTParty.post(post_uri, body: params.to_json, headers: { 'Content-Type' => 'application/json' })
+  end
 
-	def count_distance current_lat_lng, position_lat_lng
-		Geocoder::Calculations.distance_between(current_lat_lng, position_lat_lng).round(3)
-	end
+  def count_distance current_lat_lng, position_lat_lng
+    (Geocoder::Calculations.distance_between(current_lat_lng, position_lat_lng)*1000).round(3)
+  end
 
-	# redis
-	def redis_key_exist?(key)
-		$redis.exists(key.to_s)
-	end
+  # redis
+  def redis_key_exist?(key)
+    $redis.exists(key.to_s)
+  end
 
-	def redis_initialize_user(user_id)
-		$redis.set(user_id.to_s, {}.to_json)
-	end
+  def redis_initialize_user(user_id)
+    $redis.set(user_id.to_s, {}.to_json)
+  end
 
-	def redis_get_user_data(user_id)
-		JSON.parse($redis.get(user_id.to_s))
-	end
+  def redis_get_user_data(user_id)
+    JSON.parse($redis.get(user_id.to_s))
+  end
 
-	def redis_set_user_data(user_id, type, data)
-		user_data = redis_get_user_data(user_id)
-		user_data = user_data.merge(type => data)
-		$redis.set(user_id.to_s, user_data.to_json)
-	end
+  def redis_set_user_data(user_id, type, data)
+    user_data = redis_get_user_data(user_id)
+    user_data = user_data.merge(type => data)
+    $redis.set(user_id.to_s, user_data.to_json)
+  end
 
-	def get_redis_data(user_id, keyword)
-		record = redis_get_user_data(user_id)
-		data = record.dig(keyword) || ""
-	end
+  def get_redis_data(user_id, keyword)
+    record = redis_get_user_data(user_id)
+    data = record.dig(keyword) || ""
+  end
 end
--- app/services/graph_api_service.rb
+++ app/services/graph_api_service.rb
@@ -1,82 +1,84 @@
 require 'koala'
 
 class GraphApiService < BaseService
-	DEFAULT_SEARCH ||= I18n.t('settings.facebook.search_type')
-	DEFAULT_DISTANCE ||= I18n.t('settings.facebook.distance')
-	DEFAULT_MIN_SCORE ||= I18n.t('settings.facebook.score')
-	DEFAULT_FIELDS ||= I18n.t('settings.facebook.fields')
-	DEFAULT_RANDOM ||= I18n.t('settings.facebook.random')
-	DEFAULT_OPEN ||= I18n.t('settings.facebook.open_now')
+  DEFAULT_SEARCH ||= 'restaurant'
+  DEFAULT_DISTANCE ||= 500
+  DEFAULT_MIN_SCORE ||= 3.8
+  DEFAULT_RANDOM ||= true
+  DEFAULT_OPEN ||= false
+  DEFAULT_FIELDS ||= 'location,name,overall_star_rating,rating_count,
+                            phone,link,price_range,category,category_list,
+                            hours,website,is_permanently_closed'
 
-	attr_accessor :graph
-	def initialize
-		oauth_access_token = Koala::Facebook::OAuth.new.get_app_access_token
-		self.graph = Koala::Facebook::API.new(oauth_access_token)
-	end
+  attr_accessor :graph
+  def initialize
+    oauth_access_token = Koala::Facebook::OAuth.new.get_app_access_token
+    self.graph = Koala::Facebook::API.new(oauth_access_token)
+  end
 
-	def search_places lat, lng, options={}
-		limit = 100
-		user = options[:user] || nil
-		size = options[:size] || 5
-		mode = options[:mode] || nil
-		keyword = options[:keyword] || 'restaurant'
-		
-		position = "#{lat},#{lng}"
-		max_distance = user.try(:max_distance) || DEFAULT_DISTANCE
-		min_score = user.try(:min_score) || DEFAULT_MIN_SCORE
-		random_type = user.try(:random_type) || DEFAULT_RANDOM
-		open_now = user.try(:open_now) || DEFAULT_OPEN
+  def search_places lat, lng, options={}
+    limit = 100
+    user = options[:user] || nil
+    size = options[:size] || 5
+    mode = options[:mode] || nil
+    keyword = options[:keyword] || 'restaurant'
 
-		facebook_results = graph.search(keyword, type: :place, center: position, 
-			distance: max_distance, locale: I18n.locale.to_s, limit: limit, 
-			matched_categories: "FOOD_BEVERAGE", fields: DEFAULT_FIELDS)
-		# 移除連結不存在 的搜尋結果
-		# 移除類別不包含 餐 的搜尋結果
-		# 移除評分低於設定數字的搜尋結果
+    position = "#{lat},#{lng}"
+    max_distance = user.try(:max_distance) || DEFAULT_DISTANCE
+    min_score = user.try(:min_score) || DEFAULT_MIN_SCORE
+    random_type = user.try(:random_type) || DEFAULT_RANDOM
+    open_now = user.try(:open_now) || DEFAULT_OPEN
 
-		results = facebook_results.reject { |r| 
-			r['category_list'].any? {|c| REJECT_CATEGORY.any?{|n| c['name'] == n } } ||
-			r['is_permanently_closed'] == true ||
-			(r['overall_star_rating'].to_f >= 1 && r['overall_star_rating'].to_f <= min_score) }
-		# 判斷目前是否營業中
-		results = results.reject { |r| check_open_now(r['hours']) == false } if open_now
+    facebook_results = graph.search(keyword, type: :place, center: position,
+                                    distance: max_distance, locale: I18n.locale.to_s, limit: limit,
+                                    matched_categories: "FOOD_BEVERAGE", fields: DEFAULT_FIELDS)
+    # 移除連結不存在 的搜尋結果
+    # 移除類別不包含 餐 的搜尋結果
+    # 移除評分低於設定數字的搜尋結果
 
-		# 計算距離
-		results = results.each { |r| r['distance'] = (count_distance([lat, lng], [r['location']['latitude'], r['location']['longitude']])*1000).to_i }
-		results = results.reject { |r| r['distance'] > max_distance }
+    results = facebook_results.reject { |r|
+      r['category_list'].any? {|c| REJECT_CATEGORY.any?{|n| c['name'] == n } } ||
+        r['is_permanently_closed'] == true ||
+        (r['overall_star_rating'].to_f >= 1 && r['overall_star_rating'].to_f <= min_score) }
+    # 判斷目前是否營業中
+    results = results.reject { |r| check_open_now(r['hours']) == false } if open_now
 
-		results = case mode
-		when 'score'
-			results.sort_by { |r| [r['overall_star_rating'].to_f, r['rating_count'].to_i] }.reverse
-		when 'distance'
-			results = results.sort_by { |r| r['distance'] }
-		else
-			results = random_type ? results.sample(size) : results.first(size)
-		end
-	end
+    # 計算距離
+    results = results.each { |r| r['distance'] = (count_distance([lat, lng], [r['location']['latitude'], r['location']['longitude']])).to_i }
+    results = results.reject { |r| r['distance'] > max_distance }
 
-	def check_open_now hours=nil
-		open_now = false
-		if hours.present?
-			date = Time.now.strftime('%a').downcase
-			hours = hours.reject {|key, value| !key.include?(date)}
-			if hours.size > 0
-				open_time_array = []
-				(1..3).each do |i|
-					temp_array = []
-					hours.each do |key, value|
-						temp_array << value if key.include?("_#{i}_")
-					end
-					open_time_array << temp_array if temp_array.size > 0
-				end
-				current_time = Time.now.strftime('%R')
-				open_time_array.each do |time|
-					open_now = true if current_time.between?(time.first, time.last)
-				end
-			end
-		else
-			open_now = true
-		end
-		return open_now	
-	end
+    results = case mode
+              when 'score'
+                results.sort_by { |r| [r['overall_star_rating'].to_f, r['rating_count'].to_i] }.reverse
+              when 'distance'
+                results = results.sort_by { |r| r['distance'] }
+              else
+                results = random_type ? results.sample(size) : results.first(size)
+              end
+  end
+
+  def check_open_now hours=nil
+    open_now = false
+    if hours.present?
+      date = Time.now.strftime('%a').downcase
+      hours = hours.reject {|key, value| !key.include?(date)}
+      if hours.size > 0
+        open_time_array = []
+        (1..3).each do |i|
+          temp_array = []
+          hours.each do |key, value|
+            temp_array << value if key.include?("_#{i}_")
+          end
+          open_time_array << temp_array if temp_array.size > 0
+        end
+        current_time = Time.now.strftime('%R')
+        open_time_array.each do |time|
+          open_now = true if current_time.between?(time.first, time.last)
+        end
+      end
+    else
+      open_now = true
+    end
+    return open_now
+  end
 end
