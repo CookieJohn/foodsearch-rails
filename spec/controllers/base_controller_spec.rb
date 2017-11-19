e922a447e34b0becac38b2f77cf7fd0be74ec17f
John <johnwu2613@gmail.com>
2017-11-19 08:40:40 +0800

    Add rspec

--- spec/controllers/base_controller_spec.rb
+++ spec/controllers/base_controller_spec.rb
@@ -10,23 +10,37 @@ RSpec.describe BaseController, type: :request do
       end
     end
 
-    context "search" do
+    context "get location" do
       it "success" do
         get location_url
         expect(response).to have_http_status(200)
       end
     end
 
-    context "ajax" do
+    context "get selection" do
+      it "success" do
+        get selection_url
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "get restaurants" do
       let(:lat) { 25.084555 }
       let(:lng) { 121.456564 }
       let(:search_type) { "" }
 
       it "success" do
         VCR.use_cassette('facebook/search', record: :new_episodes) do
-          get results_url, params: { form: { lat: lat, lng: lng, search_type: search_type } }, xhr: true
+          get results_url, params: { form: { lat: lat, lng: lng, search_type: search_type } }
           expect(response).to have_http_status(200)
         end
+      end
+    end
+
+    context "privacy" do
+      it "success" do
+        get privacy_url
+        expect(response).to have_http_status(200)
       end
     end
   end
