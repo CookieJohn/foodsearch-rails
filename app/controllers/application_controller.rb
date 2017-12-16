3aac50b6475e1cb6032e77e8eaea5b0efa545d84
John <johnwu2613@gmail.com>
2017-12-16 08:58:17 +0800

    Add browser aliases

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+require "browser/aliases"
+Browser::Base.include(Browser::Aliases)
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
