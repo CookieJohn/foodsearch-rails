1c70c95ba21af2b3823e8435613ed82f93d9bcb0
John <johnwu2613@gmail.com>
2018-03-24 19:46:54 +0800

    Refactor API
    - upgrade api version to v2.12
    - use direct api to get more and correct infomations
    - hide jp and kr language

--- app/services/graph_api_service.rb
+++ app/services/graph_api_service.rb
@@ -11,7 +11,7 @@ class GraphApiService < BaseService
   DEFAULT_FIELDS ||= 'location,name,overall_star_rating,rating_count,
                             phone,link,price_range,category,category_list,
                             hours,website,is_permanently_closed'
-  SEARCH_API ||= 'https://graph.facebook.com/v2.11/search?'
+  SEARCH_API ||= 'https://graph.facebook.com/v2.12/search?'
 
   def initialize
     @oauth_access_token = Koala::Facebook::OAuth.new.get_app_access_token
@@ -32,26 +32,42 @@ class GraphApiService < BaseService
     random_type = user.try(:random_type) || DEFAULT_RANDOM
     open_now = open_now || DEFAULT_OPEN
 
-    facebook_results = @graph.search(keyword,
-                                     type: :place,
-                                     center: position,
-                                     distance: max_distance,
-                                     locale: I18n.locale.to_s,
-                                     limit: limit,
-                                     fields: DEFAULT_FIELDS)
+    # old API
+    # results = @graph.search(keyword,
+    #                         type: :place,
+    #                         center: position,
+    #                         distance: max_distance,
+    #                         locale: I18n.locale.to_s,
+    #                         limit: limit,
+    #                         fields: DEFAULT_FIELDS)
     # 測試API
-    # api_url = "#{SEARCH_API}q=#{keyword}&suppress_http_code=1&type=place&center=#{position}&distance=#{max_distance}&locale=#{I18n.locale}$limit=#{limit}&fields=#{DEFAULT_FIELDS}&access_token=#{@oauth_access_token}"
-    # response = Net::HTTP.get(URI.parse(api_url))
-    # facebook_results = JSON.parse(response).dig('data')
+    api_url = "#{SEARCH_API}q=#{keyword}&suppress_http_code=1&
+              type=place&
+              center=#{position}&
+              distance=#{max_distance}&
+              locale=#{I18n.locale}$
+              limit=#{limit}&
+              fields=#{DEFAULT_FIELDS}&
+              access_token=#{@oauth_access_token}"
+    response = Net::HTTP.get(URI.parse(api_url))
+    next_result_url = JSON.parse(response).dig('paging', 'next')
+    results = JSON.parse(response).dig('data')
+
+    until !next_result_url.present? do
+      response = Net::HTTP.get(URI.parse(next_result_url))
+      next_result_url = JSON.parse(response).dig('paging', 'next')
+      results += JSON.parse(response).dig('data')
+    end
 
     # 移除連結不存在 的搜尋結果
     # 移除類別不包含 餐 的搜尋結果
     # 移除評分低於設定數字的搜尋結果
 
-    results = facebook_results.reject { |r|
-      r['category_list'].any? {|c| c['name'].presence_in(REJECT_CATEGORY) } ||
-        r['is_permanently_closed'] == true ||
-        r['overall_star_rating'].to_i < min_score }
+    # results = facebook_results.reject { |r|
+    #   r['category_list'].any? {|c| c['name'].presence_in(REJECT_CATEGORY) } ||
+    #     r['is_permanently_closed'] == true ||
+    #     r['overall_star_rating'].to_i < min_score }
+    results = results.reject { |r| r['overall_star_rating'].to_f < min_score }
     # 判斷目前是否營業中
     results = results.each { |r| r['open_now'] = check_open_now(r['hours']) }
     results = results.reject { |r| r['open_now'] == false } if open_now == 'true'
