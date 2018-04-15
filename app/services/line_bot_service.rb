7a2a8c0753cf3ed4de147eb64e15342f434cd2bf
John <johnwu2613@gmail.com>
2018-04-15 20:48:52 +0800

    Refactor class

--- app/services/line_bot_service.rb
+++ app/services/line_bot_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'line/bot'
 
 class LineBotService < BaseService
@@ -5,10 +7,10 @@ class LineBotService < BaseService
   include Conversion
 
   def initialize(request)
-    @line_client ||= Line::Bot::Client.new { |config|
+    @line_client ||= Line::Bot::Client.new.tap do |config|
       config.channel_secret = ENV['line_channel_secret']
       config.channel_token  = ENV['line_channel_token']
-    }
+    end
     @graph   ||= GraphApiService.new
     @google  ||= GoogleMapService.new
     @request ||= request
@@ -20,40 +22,39 @@ class LineBotService < BaseService
 
     body = @request.body.read
     events = @line_client.parse_events_from(body)
-    events.each { |event|
+    events.each do |event|
       # find_line_user(event['source']['userId'])
 
       case event
       when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          # msg = event.message['text'].downcase
-          # @line_client.reply_message(event['replyToken'], text_format(msg))
-        when Line::Bot::Event::MessageType::Location
-          lat = event.message['latitude']
-          lng = event.message['longitude']
-          facebook_results = @graph.search_places(lat, lng, user: @user)
-          if facebook_results.size > 0
-            options = carousel_options(facebook_results)
-            return_response = carousel_format(options)
-          else
-            return_response = text_format(I18n.t('empty.restaurants'))
-          end
-          @line_client.reply_message(event['replyToken'], return_response)
-        end
+        reply = case event.type
+                when Line::Bot::Event::MessageType::Text
+                  # msg = event.message['text'].downcase
+                  # text_format(msg)
+                when Line::Bot::Event::MessageType::Location
+                  lat = event.message['latitude']
+                  lng = event.message['longitude']
+                  facebook_results = @graph.search_places(lat, lng, user: @user)
+
+                  return text_format(I18n.t('empty.restaurants')) if facebook_results.empty?
+
+                  options = carousel_options(facebook_results)
+                  carousel_format(options)
+                end
+
+        @line_client.reply_message(event['replyToken'], reply)
       end
-    }
-    200
+    end
   end
 
   def varify_signature
     body = @request.body.read
-    signature = @request.env['HTTP_X_LINE_SIGNATURE']
-    return '400 Bad Request' unless @line_client.validate_signature(body, signature)
+    sign = @request.env['HTTP_X_LINE_SIGNATURE']
+    return '400 Bad Request' unless @line_client.validate_signature(body, sign)
   end
 
-  def find_line_user id
-    User.create!(line_user_id: id) if !User.exists?(line_user_id: id)
+  def find_line_user(id)
+    User.create!(line_user_id: id) unless User.exists?(line_user_id: id)
     @user = User.find_by(line_user_id: id)
   end
 end
