fa6c5cd5670c9cd2761e90c7a6363feea90c3765
John <johnwu2613@gmail.com>
2018-04-27 10:44:18 +0800

    Change to 3.10.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.10.1"
+lock "3.10.2"
 
 set :application, "foodsearch"
 set :repo_url, "git@github.com:CookieJohn/foodsearch-rails.git"
