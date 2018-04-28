d888a809ea3f79bc8613127f3dc0bad7d596cc96
John <johnwu2613@gmail.com>
2018-04-28 20:27:03 +0800

    Restore redis function

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -15,43 +15,38 @@ class BaseService
   def http_get get_uri, params=nil
     uri = safe_url(get_uri)
     res = Net::HTTP.get_response(uri)
-    results = JSON.parse(res.body)['results']
+    JSON.parse(res.body)['results']
   end
 
   def http_post post_uri, params
-    res = HTTParty.post(post_uri, body: params.to_json, headers: { 'Content-Type' => 'application/json' })
+    HTTParty.post(post_uri, body: params.to_json, headers: { 'Content-Type' => 'application/json' })
   end
 
   def count_distance current_lat_lng, position_lat_lng
-    (Geocoder::Calculations.distance_between(current_lat_lng, position_lat_lng)*1000).round(3)
+    (Geocoder::Calculations.distance_between(current_lat_lng, position_lat_lng) * 1000).round(3)
   end
 
   # redis
   def redis_key_exist?(key)
-    # $redis.exists(key.to_s)
-    false
+    $redis.exists(key.to_s)
   end
 
   def redis_initialize_user(user_id)
-    # $redis.set(user_id.to_s, {}.to_json)
-    ""
+    $redis.set(user_id.to_s, {}.to_json)
   end
 
   def redis_get_user_data(user_id)
-    # JSON.parse($redis.get(user_id.to_s))
-    ""
+    JSON.parse($redis.get(user_id.to_s))
   end
 
   def redis_set_user_data(user_id, type, data)
-    # user_data = redis_get_user_data(user_id.to_s)
-    # user_data = user_data.merge(type => data)
-    # $redis.set(user_id.to_s, user_data.to_json)
-    ""
+    user_data = redis_get_user_data(user_id.to_s)
+    user_data = user_data.merge(type => data)
+    $redis.set(user_id.to_s, user_data.to_json)
   end
 
   def get_redis_data(user_id, keyword)
-    # record = redis_get_user_data(user_id)
-    # data = record.dig(keyword) || ""
-    ""
+    record = redis_get_user_data(user_id)
+    record.dig(keyword) || ''
   end
 end
