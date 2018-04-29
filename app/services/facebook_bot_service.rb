611648ce43083f907500763c90612b9ac82518b5
John <johnwu2613@gmail.com>
2018-04-29 22:36:24 +0800

    Fix can not call private method

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -190,15 +190,15 @@ class FacebookBotService < BaseService
       @title   = ''
     end
 
-    private
-
-    def self.reply
+    def reply
       title   = '請選擇搜尋方式，設定頁面可以調整搜尋條件。'
       options << button_option('postback', '選擇搜尋類型', 'choose_search_type')
       options << button_option('postback', '關鍵字搜尋', 'customized_keyword')
       options << button_link_option("https://johnwudevelop.tk/users/#{@user_id}", '搜尋設定')
       button_format(title, options)
     end
+
+    private
 
     def choose_search_reply
       quick_replies_option(I18n.t('messenger.re-select'), 'choose_search_type')
@@ -210,9 +210,7 @@ class FacebookBotService < BaseService
   end
 
   class NoResult < DefaultResponse
-    private
-
-    def self.reply
+    def reply
       title   = "這個位置，沒有與#{get_redis_data(@user_id, 'keyword')}相關的搜尋結果！"
       options = [choose_search_reply, back_reply]
       quick_replies_format(title, options)
@@ -220,9 +218,7 @@ class FacebookBotService < BaseService
   end
 
   class SearchDone < DefaultResponse
-    private
-
-    def self.reply
+    def reply
       title = '有找到喜歡的嗎？'
       options << quick_replies_option(I18n.t('messenger.enter-keyword'), 'customized_keyword')
       options << choose_search_reply
