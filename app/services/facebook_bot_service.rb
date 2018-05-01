533ae90c9d8aad24a043f4a2cd98192dfb02c212
John <johnwu2613@gmail.com>
2018-05-01 20:28:10 +0800

    Fix

--- app/services/facebook_bot_service.rb
+++ app/services/facebook_bot_service.rb
@@ -23,6 +23,7 @@ class FacebookBotService < BaseService
 
     entries.each do |entry|
       entry['messaging'].each do |receive_message|
+        return
         return if is_bot?(receive_message)
 
         message      = receive_message.dig('message', 'text')
@@ -35,10 +36,14 @@ class FacebookBotService < BaseService
                          location_setting(receive_message)
                          return search_by_location
                        when receive_message.dig('message', 'text')
-                         (get_redis_data(@user_id, 'customize') == true) ? 'search_specific_item' : 'message'
+                         if get_redis_data(@user_id, 'customize') == true
+                           'search_specific_item'
+                         else
+                           'message'
+                         end
                        end
 
-        message_data = get_response(message_type, message) if message_type.present?
+        message_data = get_response(message_type, message)
         http_post(API_URL, message_data) if message_data.present?
       end
     end
